3b72119618e1cda68f0f0da975679ff28581ff45
Jivko Uzunov <59103450+jivko25@users.noreply.github.com>
2020-03-03 19:29:09 +0200

    Update Application_main.java

--- Smoko/src/Application_main.java
+++ Smoko/src/Application_main.java
@@ -7,10 +7,11 @@ public class Application_main {
 	public static void main(String[] args) {
 		JFrame windowFrame = new JFrame();
 		windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		windowFrame.setSize(800, 800);
+		windowFrame.setSize(915, 915);
 		windowFrame.getContentPane().setBackground(Color.GREEN);
-		windowFrame.add(new Snake_Pieces());
+		windowFrame.add(new GameBoard());
 		windowFrame.setVisible(true);
+		windowFrame.setResizable(false);
 	}
 
 }
