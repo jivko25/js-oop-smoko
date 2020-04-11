b489a286555fd1a01d632dedf5bd30118ad72985
Jivko Uzunov <59103450+jivko25@users.noreply.github.com>
2020-04-11 06:15:16 +0300

    Update Application_main.java

--- Smoko/src/Application_main.java
+++ Smoko/src/Application_main.java
@@ -7,7 +7,7 @@ public class Application_main {
 	public static void main(String[] args) {
 		JFrame windowFrame = new JFrame();
 		windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		windowFrame.setSize(915, 915);
+		windowFrame.setSize(915, 715);
 		windowFrame.getContentPane().setBackground(Color.GREEN);
 		windowFrame.add(new GameBoard());
 		windowFrame.setVisible(true);
